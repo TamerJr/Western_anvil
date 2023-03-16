62b7da690aca8ad26a1275726596518b6dd56440
TamerJr <ahmttmer@gmail.com>
2023-03-16 15:20:52 +0200

    Collect Eles In Same Files

--- Western_anvil/src/Utilies/utilies.js
+++ Western_anvil/src/Utilies/utilies.js
@@ -1,1 +1,20 @@
+// Components
+
+
 export {default as Navbar} from "../Components/Navbar/Navbar"
+export {default as Form} from "../Components/Form/Form"
+export {default as ShopCard} from "../Components/ShopCard/ShopCard"
+export {default as Navbar} from "../Components/TermsSection/TermsSection"
+export {default as ThemeToggler} from "../Components/ThemeToggler/ThemeToggler"
+export {default as ValueSection} from "../Components/ValueSection/ValueSetion"
+
+// Pages
+
+export {default as AboutUs} from "../Pages/AboutUs/AboutUs"
+export {default as Home} from "../Pages/Home/Home"
+export {default as LogIn} from "../Pages/LogIn/LogIn"
+export {default as SignUp} from "../Pages/SignUp/SignUp"
+export {default as Shop} from "../Pages/Shop/Shop"
+export {default as Terms} from "../Pages/Terms/Terms"
+export {default as ShopCart} from "../Pages/ShopCart/ShopCart"
+export {default as VedioDetails} from "../Pages/VedioDEtails/VedioDEtails"
